a71d6d32d4ba8a53439aeac60b731c35fa1dcbb1
Surge Ali <surgeali@gmail.com>
2019-08-21 10:49:32 -0700

    funtioning record, stop and play buttons

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,38 @@ import Keyboard from "./components/keyboard";
 import './index.css'
 import NavBar from "./components/navbar";
 
-
+const PLAY = 'play';
+const RECORD = 'record';
+const STOP = 'stop';
+const SAVE = 'save';
 
 // const src = "https://images.unsplash.com/photo-1509335919466-c196457ea95a?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80";
 
 class App extends Component {
   state = {
-    record: false
+    mode: STOP
   }
   recordPressed = () => {
-    this.setState({ record: true })
+    this.setState({ mode: RECORD })
+  }
+  playPressed = () => {
+    console.log('playPressed')
+    this.setState({ mode: PLAY })
+  }
+  stopPressed = () => {
+    this.setState({ mode: STOP })
+  }
+  savePressed = () => {
+    this.setState({ mode: SAVE })
   }
   render() {
     return (
       <div>
-        <NavBar recordPressed={this.recordPressed} playPressed={this.playPressed} />
+        <NavBar recordPressed={this.recordPressed} playPressed={this.playPressed} stopPressed={this.stopPressed} savePressed={this.savePressed} />
         <Router>
           <div>
             <Route exact path="/" component={Login} />
-            <Route exact path="/keyboard" render={() => (<Keyboard record={this.state.record} />)}/>
+            <Route exact path="/keyboard" render={() => (<Keyboard mode={this.state.mode} />)} />
           </div>
         </Router>
       </div>
