772914b912065f78750c4f3fd86f874e2b32d823
Surge Ali <surgeali@gmail.com>
2019-08-26 12:19:25 -0700

    front-end login ready

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,7 +7,25 @@ export default {
     // },
     
     //Save user to database
-    saveUser: function(username) {
-        return axios.post("/api/users", userData);
+    saveUser: function(user) {
+        console.log('user', user);
+        return axios.post("/api/users", user);
     }
 }
+
+// doubleNumber: function(x, y) {
+//     return x*2 + " " + y
+// }
+
+// const doubleNumber = function () {
+//     return x*2
+// }
+
+// function doubleNumber() {
+//     return x*2
+// }
+
+// const doubleNumber = x => x*2;
+
+// let num = 10;
+// doubleNumber('apples', 5);
